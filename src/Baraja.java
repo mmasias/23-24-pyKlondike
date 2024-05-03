07a24b02d256b38704f2185fe54d5a6b6c4584e0
manuel@-2.6 <manuel@masiasweb.com>
2024-05-03 12:07:33 +0200

    Cuarto paso...

--- src/Baraja.java
+++ src/Baraja.java
@@ -1,9 +1,48 @@
+import java.util.Random;
 
 public class Baraja {
 
+    private Carta[] cartas;
+    private int ultima;
+
+    public Baraja(){
+        ultima = 0;
+        cartas = new Carta[52];
+        for(int palo=0;palo<4;palo++){
+            for(int numero=0;numero<13;numero++){
+                this.poner(new Carta(palo, numero));
+            }
+        }
+        this.barajar();
+    }
+
+    private void barajar() {
+
+        Random aleatorio = new Random();
+        for(int i = 0; i<1000; i++){
+            int origen = aleatorio.nextInt(52);
+            int destino = aleatorio.nextInt(52);
+            Carta carta = cartas[origen];
+            cartas[origen] = cartas[destino];
+            cartas[destino] = carta;
+        }
+    }
+
+    private void poner(Carta carta) {
+        cartas[ultima]=carta;
+        ultima++;
+    }
+
     public void mostrar() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'mostrar'");
+        System.out.println("BARAJA: ");
+        
+        Carta carta = this.cima();
+        carta.mostrar();
+
+    }
+
+    private Carta cima() {
+        return cartas[ultima-1];
     }
 
     public void moverA(Descarte descarte) {
