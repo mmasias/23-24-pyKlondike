158f169c42f0b90e8bcf78152e98de3ed1e58172
manuel@AIO.universidad <manuel@masiasweb.com>
2024-05-07 17:43:39 +0200

    1st try

--- src/Mano.java
+++ src/Mano.java
@@ -1,8 +1,49 @@
 
 public class Mano {
 
+    private Carta[] cartas;
+    private int ultima;
+    private boolean abierta;
+
     public Mano(Baraja baraja) {
-        //TODO Auto-generated constructor stub
+        ultima = 0;
+        cartas = new Carta[10];
+        for (int i = 0; i < 2; i++) {
+            Carta carta = baraja.sacar();
+            carta.voltear();
+            this.poner(carta);
+        }        
+        this.abierta=true;
     }
 
+    private void poner(Carta carta) {
+        cartas[ultima] = carta;
+        ultima++;
+    }
+
+    public void mostrar() {
+        System.out.print("Mano: ");
+        if (this.vacia()) {
+            System.out.println("NADA");
+        } else {
+            for (int i = 0; i < ultima; i++) {
+                cartas[i].mostrar();
+            }
+        }
+        System.out.println();
+    }
+
+    private boolean vacia() {
+        return ultima == 0;
+    }
+
+    public void pedir(Baraja baraja) {
+        Carta carta = baraja.sacar();
+        carta.voltear();
+        this.poner(carta);        
+    }
+
+    public void cerrar() {
+        this.abierta = false;
+    }
 }
--- src/XXI.java
+++ src/XXI.java
@@ -1,10 +1,40 @@
+import java.util.Scanner;
+
 class XXI {
 
+    private Baraja baraja;
+    private Mano mano;
 
+    public XXI() {
+        baraja = new Baraja();
+        mano = new Mano(baraja);
+    }
 
     private void jugar() {
+        boolean estaJugando = true;
 
-    }  
+        do {
+            mano.mostrar();
+            mostrarMenu();
+            switch (new Scanner(System.in).nextInt()) {
+                case 1:
+                    mano.pedir(baraja);
+                    break;
+                case 2:
+                    mano.cerrar();
+                    break;
+                case 3:
+                    mano = new Mano(baraja);
+                    break;
+            }
+        } while (estaJugando);
+    }
+
+    private void mostrarMenu() {
+        System.out.println("1. Pedir");
+        System.out.println("2. Plantar");
+        System.out.println("3. Empezar de nuevo");
+    }
 
     public static void main(String[] args) {
         new XXI().jugar();
