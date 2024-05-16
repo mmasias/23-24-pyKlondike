bb74eea5bf4b2fb4ffe2dba62170acaeb765cf88
Sergio Mayen <sergio.mayen@ct.uneatlantico.es>
2024-05-16 21:18:18 +0200

    Avance1: Cambiar los simbolos en la clase Carta por los nombres, Corazones, Trebol.... etc y ajuntar archivo de trabajo

---
+++ src/RetoEDA.java
@@ -0,0 +1,22 @@
+import java.util.Arrays;
+import java.util.Comparator;
+
+public class RetoEDA {
+
+    private Baraja baraja;
+
+    public static void main(String[] args) {
+
+        Baraja baraja = new Baraja();
+
+        //
+            
+        //
+        Carta carta;
+        do {
+            carta = baraja.sacar();
+            carta.voltear();
+            carta.mostrar();
+        } while (!baraja.vacia());
+    }
+}
